f93a9d6c8929fb01ed173efb7526da75c238c7fa
Krishna Adhikari <krishna.forkhaki@gmail.com>
2023-04-27 19:06:41 +0545

    CRUD using firebase done

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,7 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
   FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  getPhoneDirectoryCollectionReference() {
+    return firestore
+        .collection("users")
+        .doc(firebaseAuth.currentUser!.uid)
+        .collection("phone_directory");
+  }
+
+  Future addPhoneDirectory(Map<String, dynamic> data) async {
+    try {
+      await getPhoneDirectoryCollectionReference().add(data);
+    } catch (e) {
+      e.printError();
+    }
+  }
+
+  Future updatePhoneDirectory(
+    Map<String, dynamic> data,
+    String documentId,
+  ) async {
+    try {
+      await getPhoneDirectoryCollectionReference().doc(documentId).update(data);
+    } catch (e) {
+      e.printError();
+    }
+  }
+
+  Future deletePhoneDirectory(String documentId) async {
+    try {
+      await getPhoneDirectoryCollectionReference().doc(documentId).delete();
+    } catch (e) {
+      e.printError();
+    }
+  }
+
+  Stream<QuerySnapshot> get phoneDirectoryStream =>
+      getPhoneDirectoryCollectionReference().snapshots();
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -13,11 +13,67 @@ class HomeView extends GetView<HomeController> {
         title: const Text('HomeView'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          controller.firebaseAuth.currentUser!.email.toString(),
-          style: const TextStyle(fontSize: 20),
-        ),
+      body: StreamBuilder(
+        stream: controller.phoneDirectoryStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(snapshot.error.toString()),
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              Map<String, dynamic> data =
+                  snapshot.data!.docs[index].data()! as Map<String, dynamic>;
+
+              return ListTile(
+                title: Text(data["name"]),
+                subtitle: Text(data["phone"]),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      onPressed: () async {
+                        Map<String, dynamic> phoneBook = {
+                          "name": "updated Abc Xyzafdasd",
+                          "phone": "9700000080"
+                        };
+                        String docId = snapshot.data!.docs[index].id;
+
+                        await controller.updatePhoneDirectory(phoneBook, docId);
+                      },
+                      icon: const Icon(Icons.edit),
+                    ),
+                    IconButton(
+                      onPressed: () async {
+                        String docId = snapshot.data!.docs[index].id;
+                        await controller.deletePhoneDirectory(docId);
+                      },
+                      icon: const Icon(Icons.delete),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          Map<String, dynamic> phoneBook = {
+            "name": "Abc Xyzafdasd",
+            "phone": "9800000080"
+          };
+          await controller.addPhoneDirectory(phoneBook);
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
